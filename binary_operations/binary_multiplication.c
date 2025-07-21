7aefe7eb35a36ac6000d4fadc9c77e820446cec5
Dinesh B <iamdineshbasnet@gmail.com>
2025-07-21 23:34:47 +0545

    feat(binary_operations): binary multiplication

---
+++ binary_operations/binary_multiplication.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "binary_utils.h"
+
+int main()
+{
+  char bin1[64], bin2[64], result[64];
+
+  printf("Enter first binary number: ");
+  scanf("%s", bin1);
+
+  printf("Enter second binary number: ");
+  scanf("%s", bin2);
+
+  int a = binaryToDecimal(bin1);
+  int b = binaryToDecimal(bin2);
+
+  decimalToBinary(a * b, result);
+  printf("Multiplication: %s * %s = %s\n", bin1, bin2, result);
+
+  return 0;
+}
